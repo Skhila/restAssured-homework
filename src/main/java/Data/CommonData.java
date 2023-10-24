e590004ef8b3fc4fd8288dd03baf55f977feb9d2
Skhila <bachanaskhiladze@gmail.com>
2023-10-24 15:11:47 +0400

    Created Tests Of BookStore API using RestAssured

---
+++ src/main/java/Data/CommonData.java
@@ -0,0 +1,15 @@
+package Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CommonData {
+
+    public static final String bookStoreGetURLWithoutParams = "https://bookstore.toolsqa.com/BookStore/v1/Books";
+
+    public static final String bookStoreGetURLWithParams = "https://bookstore.toolsqa.com/BookStore/v1/Book";
+
+    public static List<Map<String, String>> extractedBooks = new ArrayList<>();
+
+}
